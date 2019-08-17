40e4043dbc775a3f308df2ee55f15c13f8257e9c
Giovanni Gobbi <ggobbi@amazon.com>
2019-08-17 16:29:05 +0200

    Edit error message

--- parse_message.c
+++ parse_message.c
@@ -53,7 +53,7 @@ int parse_and_check_measurement_message(char *buffer, hello_message request, mea
     printf("%s %lu", payload, strlen(payload)); fflush(stdout);
     message->probe_seq_num = probe_seq_num;
     if(strlen(payload) != request.msg_size) {
-        fprintf(stderr, "Invalid input message: payload size must be exactly the specified msg_size: %lu, %d", strlen(payload), request.msg_size);
+        fprintf(stderr, "Invalid input message: payload size must be exactly the specified msg_size: expected %d, actual %lu", request.msg_size, strlen(payload));
         return 1;
     }
     if(message->payload == NULL) {

18bcfa31a7c376001e054a46823f936cbcbefff4
Matteo Fiordarancio <mfiordar@amazon.com>
2019-08-18 18:27:58 +0200

    Kept working on refactoring, now much more readable

--- client.c
+++ client.c
@@ -36,12 +36,13 @@ int main(int argc, char *argv[]) {
     if(mode == RTT) {
         int sizes[6] = {1, 100, 200, 400, 800, 1000};
         int n_measurements_for_size = 20;
-        // execute_rtt(argv[1], argv[2]);
-        execute_request(RTT, sizes, 6, n_measurements_for_size);
+        execute_rtt(argv[1], argv[2]);
+        //execute_request(RTT, sizes, 6, n_measurements_for_size);
     } else if(mode == THROUGHPUT) {
         int measurements[5] = {1000, 2000, 4000, 16000, 32000};
         int n_measurements_for_size = 20;
         // execute_throughput(argv[1], argv[2]);
+        execute_throughput(argv[1], argv[2]);
     } else { 
         fprintf(stderr, "Invalid mode");
         return 1;
@@ -49,90 +50,90 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-int execute_request(int operation, int sizes[], int sizes_length, int n_measurements_for_size) {
-    char *hello_op = NULL;
-    if(operation == RTT) {
-        hello_op = "rtt";
-    } else if(operation == THROUGHPUT) {
-        hello_op = "thput";
-    } else {
-        return 1;
-    }
-    ssize_t recv_message_size;
-    
-    struct timespec start, stop;
-    
-    double total_measurements[sizes_length * n_measurements_for_size];
-    int max_size = max_array(sizes, sizes_length);
-    char *string = get_string_by_length(max_size);
-
-    char message_recv[max_size];
-
-    for(int i = 0; i < sizes_length; i++) {
-        int sfd = socket(AF_INET, SOCK_STREAM, 0);    
-        if (sfd < 0){
-            fprintf(stderr, "Socket creation failed: errno value is %d\n", errno);
-            return 2;
-        }
-        struct sockaddr_in server_addr;
-        server_addr.sin_family = AF_INET;
-        server_addr.sin_addr.s_addr = inet_addr(ip_address);
-        server_addr.sin_port = htons(atoi(port));
-        
-        int cr = connect(sfd, (struct sockaddr *) &server_addr, sizeof(server_addr));
-        if (cr == -1) {
-            fprintf(stderr, "Connect operation failed: errno value is %d\n", errno);
-            return 2;
-        }
-
-        //Hello phase
-        char message_send[max_size];
-        sprintf(message_send, "h %s %d %d 0", hello_op, n_measurements_for_size, sizes[i]);
-        send(sfd, message_send, strlen(message_send), 0);
-        recv_message_size = recv(sfd, message_recv, INVALID_HELLO_SIZE, 0);
-        printf("Hello return message from the server: "); ff;
-        print_string(message_recv); ff;
-        if(strcmp(message_recv, "200 OK - Ready") != 0) {
-            fprintf(stderr, "Invalid Hello Message");
-            return 1;
-        }
-        int size = sizes[i];
-
-        //Measurements phase
-        for(int j = 1; j <= 20; j++) {
-            char data[1024];
-            sprintf(data, "m %d %.*s", j, size, string);
-            printf("Message: %s\n", data);
-            printf("String of length %d going to be sent to server. Data lenght: %lu\n", size, strlen(data)); ff;
-
-            clock_gettime(CLOCK_REALTIME, &start);
-            send(sfd, data, strlen(data), 0);
-            
-            ssize_t recv_message_sizes = recv(sfd, message_recv, MAX_MESSAGE_SIZE, 0);
-            clock_gettime(CLOCK_REALTIME, &stop);
-            printf("Measurement return message from the server: "); ff;
-            print_string(message_recv); ff;
-            // if(recv_message_sizes != size) {
-            //     print_string(message_recv); ff;
-            //     exit(1);
-            // }
-            // if(operation == RTT) {
-            //     total_measurements[i * j + j] = ((double) (stop.tv_nsec - start.tv_nsec) / 1E6);
-            //     printf("Measured RTT: %lf ms.\n", total_measurements[i * j + j]); ff;
-            // } else {
-            //     total_measurements[i * j + j] = size / ((double) (stop.tv_nsec - start.tv_nsec) / 1E9);
-            //     printf("Measured throughput: %lf b/s.\n", total_measurements[i * j + j]); ff;
-            // }
-        }
-
-        //Bye phase
-        sprintf(message_send, "b");
-        send(sfd, message_send, 50000, 0);
-        close(sfd);
-    }
-
-    return 0;
-}
+//int execute_request(int operation, int sizes[], int sizes_length, int n_measurements_for_size) {
+//    char *hello_op = NULL;
+//    if(operation == RTT) {
+//        hello_op = "rtt";
+//    } else if(operation == THROUGHPUT) {
+//        hello_op = "thput";
+//    } else {
+//        return 1;
+//    }
+//    ssize_t recv_message_size;
+//
+//    struct timespec start, stop;
+//
+//    double total_measurements[sizes_length * n_measurements_for_size];
+//    int max_size = max_array(sizes, sizes_length);
+//    char *string = get_string_by_length(max_size);
+//
+//    char message_recv[max_size];
+//
+//    for(int i = 0; i < sizes_length; i++) {
+//        int sfd = socket(AF_INET, SOCK_STREAM, 0);
+//        if (sfd < 0){
+//            fprintf(stderr, "Socket creation failed: errno value is %d\n", errno);
+//            return 2;
+//        }
+//        struct sockaddr_in server_addr;
+//        server_addr.sin_family = AF_INET;
+//        server_addr.sin_addr.s_addr = inet_addr(ip_address);
+//        server_addr.sin_port = htons(atoi(port));
+//
+//        int cr = connect(sfd, (struct sockaddr *) &server_addr, sizeof(server_addr));
+//        if (cr == -1) {
+//            fprintf(stderr, "Connect operation failed: errno value is %d\n", errno);
+//            return 2;
+//        }
+//
+//        //Hello phase
+//        char message_send[max_size];
+//        sprintf(message_send, "h %s %d %d 0", hello_op, n_measurements_for_size, sizes[i]);
+//        send(sfd, message_send, strlen(message_send), 0);
+//        recv_message_size = recv(sfd, message_recv, INVALID_HELLO_SIZE, 0);
+//        printf("Hello return message from the server: "); ff;
+//        print_string(message_recv); ff;
+//        if(strcmp(message_recv, "200 OK - Ready") != 0) {
+//            fprintf(stderr, "Invalid Hello Message");
+//            return 1;
+//        }
+//        int size = sizes[i];
+//
+//        //Measurements phase
+//        for(int j = 1; j <= 20; j++) {
+//            char data[1024];
+//            sprintf(data, "m %d %.*s", j, size, string);
+//            printf("Message: %s\n", data);
+//            printf("String of length %d going to be sent to server. Data lenght: %lu\n", size, strlen(data)); ff;
+//
+//            clock_gettime(CLOCK_REALTIME, &start);
+//            send(sfd, data, strlen(data), 0);
+//
+//            ssize_t recv_message_sizes = recv(sfd, message_recv, MAX_MESSAGE_SIZE, 0);
+//            clock_gettime(CLOCK_REALTIME, &stop);
+//            printf("Measurement return message from the server: "); ff;
+//            print_string(message_recv); ff;
+//            // if(recv_message_sizes != size) {
+//            //     print_string(message_recv); ff;
+//            //     exit(1);
+//            // }
+//            // if(operation == RTT) {
+//            //     total_measurements[i * j + j] = ((double) (stop.tv_nsec - start.tv_nsec) / 1E6);
+//            //     printf("Measured RTT: %lf ms.\n", total_measurements[i * j + j]); ff;
+//            // } else {
+//            //     total_measurements[i * j + j] = size / ((double) (stop.tv_nsec - start.tv_nsec) / 1E9);
+//            //     printf("Measured throughput: %lf b/s.\n", total_measurements[i * j + j]); ff;
+//            // }
+//        }
+//
+//        //Bye phase
+//        sprintf(message_send, "b");
+//        send(sfd, message_send, 50000, 0);
+//        close(sfd);
+//    }
+//
+//    return 0;
+//}
 
 char * get_string_by_length(int length) {
     char *string = NULL;
--- client_executor.c
+++ client_executor.c
@@ -10,25 +10,43 @@
 #include <netinet/in.h>
 
 #define ff fflush(stdout);
+#define TRIES 20
+#define MAX_SIZE 32100
+#define RTT 1
+#define THROUGHTPUT 2
+#define TO_MILLISECONDS 1000000000
 
 char * get_string_by_length(int length);
 
-int execute_rtt(char *ip_address, char *port) {
+void print_average_rtt(long rtts[], int array_length){
+    long total = 0;
+    for(int i=0; i < array_length; i++){
+        total += rtts[i];
+    }
+    double average = (double) total / array_length;
+    printf("Average RTT: %e\n", (average / TO_MILLISECONDS));
+}
 
-    double rtts[120];
+void print_average_throughtput(long rtts[]){
+    //TODO: calculate throughput
+}
+
+void execute(char * port, char * ip_address, int measurements[], int measurements_size, int mode){
+    int number_of_measurements = TRIES * measurements_size;
+    long rtts[number_of_measurements];
     
-    char receivedData[1024];
-    int measurements[6] = {1, 100, 200, 400, 800, 1000};
+    char rcv_buffer[MAX_SIZE];
+    char send_buffer[MAX_SIZE];
     struct timespec start, stop;
     
-    char *string = get_string_by_length(1000);
-    for(int i = 0; i < 6; i++) {
-        int sfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);    
+    char *string = get_string_by_length(MAX_SIZE);
+    for(int i = 0; i < measurements_size; i++) {
+        int sfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
         if (sfd < 0){
             perror("socket"); // Print error message
-            return 1;
+            exit(1);
         }
-
+        
         struct sockaddr_in server_addr;
         server_addr.sin_family = AF_INET;
         server_addr.sin_addr.s_addr = inet_addr(ip_address);
@@ -38,103 +56,51 @@ int execute_rtt(char *ip_address, char *port) {
             perror("connect operation failed");
             exit(EXIT_FAILURE);
         }
-
-        char str[80];
-        sprintf(str, "h rtt 20 %d 0", measurements[i]);
-        send(sfd, str, 80, 0);
-        recv(sfd, receivedData, 1000, 0);
+        
+        sprintf(send_buffer, "h %s 20 %d 0", mode == RTT ? "rtt" : "thput", measurements[i]);
+        send(sfd, send_buffer, strlen(send_buffer), 0);
+        recv(sfd, rcv_buffer, MAX_SIZE, 0);
         int size = measurements[i];
-        for(int j = 1; j <= 20; j++) {
-            char data[1024];
-            sprintf(data, "m %d %.*s", j, size, string);
-            printf("%s\n", data); ff;
+        for(int j = 1; j <= TRIES; j++) {
+            sprintf(send_buffer, "m %d %.*s", j, size, string);
             printf("String of length %d going to be sent to server.\n", size); ff;
-
+            
             clock_gettime(CLOCK_REALTIME, &start);
-            send(sfd, data, strlen(data), 0);
-            ssize_t byteRecv = recv(sfd, receivedData, 1024, 0);
+            send(sfd, send_buffer, strlen(send_buffer), 0);
+            ssize_t byteRecv = recv(sfd, rcv_buffer, MAX_SIZE, 0);
             clock_gettime(CLOCK_REALTIME, &stop);
             printf("%d %zd \n", size, byteRecv); ff;
-            if((int) byteRecv != size) {
-                // printData(receivedData, byteRecv); ff;
+            if(byteRecv != size) {
+                // printData(rcv_buffer, byteRecv); ff;
                 exit(1);
             }
-            rtts[(i + 1) * j + j] = (double) (stop.tv_nsec - start.tv_nsec);
-            printf("Time: %lf\n", rtts[(i + 1) * j + j]); ff;
+            long current_rtt = stop.tv_nsec - start.tv_nsec;
+            rtts[(i + 1) * j + j] = current_rtt;
         }
-        char data[1024];
-        sprintf(data, "b");
-        send(sfd, data, 1024, 0);
+        sprintf(send_buffer, "b");
+        send(sfd, send_buffer, MAX_SIZE, 0);
         close(sfd);
     }
-
-    double total_time = 0;
-    for(int i = 0; i < 120; i++) {
-        total_time += rtts[i];
+    sleep(1);
+    if (mode == RTT) {
+        print_average_rtt(rtts, number_of_measurements);
+    }else{
+        // CHANGE
+        print_average_rtt(rtts, number_of_measurements);
     }
-    printf("RTT: %lf ms\n", total_time / (120 * 1000000)); ff;
+    
+    exit(0);
+}
 
-    return 0;
+
+int execute_rtt(char *ip_address, char *port) {
+    int measurements[6] = {1, 100, 200, 400, 800, 1000};
+    execute(port, ip_address, measurements, 6, RTT);
+    exit(0);
 }
 
 int execute_throughput(char *ip_address, char *port) {
-
-    double rtts[100];
-    
-    char data[50000];
     int measurements[5] = {1000, 2000, 4000, 16000, 32000};
-    struct timespec start, stop;
-    
-    char *string = get_string_by_length(50000);
-    for(int i = 0; i < 5; i++) {
-
-        int sfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);    
-        if (sfd < 0){
-            perror("socket"); // Print error message
-            return 1;
-        }
-
-        struct sockaddr_in server_addr;
-        server_addr.sin_family = AF_INET;
-        server_addr.sin_addr.s_addr = inet_addr(ip_address);
-        server_addr.sin_port = htons(atoi(port));
-        int cr = connect(sfd, (struct sockaddr *) &server_addr, sizeof(server_addr));
-        if (cr == -1) {
-            perror("connect operation failed");
-            exit(EXIT_FAILURE);
-        }
-
-        char str[80];
-        sprintf(str, "h thput 20 %d 0", measurements[i]);
-        send(sfd, str, 80, 0);
-        recv(sfd, data, 1000, 0);
-        int size = measurements[i];
-        for(int j = 1; j <= 20; j++) {
-            sprintf(data, "m %d %.*s", j, size, string);
-            printf("String of length %d going to be sent to server. Data lenght: %lu\n", size, strlen(data)); ff;
-
-            clock_gettime(CLOCK_REALTIME, &start);
-            send(sfd, data, strlen(data), 0);
-            ssize_t byteRecv = recv(sfd, data, 32800, 0);
-            clock_gettime(CLOCK_REALTIME, &stop);
-            printf("%d %zd \n", size, byteRecv); ff;
-            if((int) byteRecv != size) {
-                // printData(data, byteRecv); ff;
-                exit(1);
-            }
-            rtts[(i + 1) * j + j] = size / ((double) (stop.tv_nsec - start.tv_nsec) / 1000000000);
-            printf("Time: %lf\n", rtts[(i + 1) * j + j]); ff;
-        }
-        sprintf(data, "b");
-        send(sfd, data, 50000, 0);
-        close(sfd);
-    }
-
-    double total_time = 0;
-    for(int i = 0; i < 100; i++) {
-        total_time += rtts[i];
-    }
-    printf("RTT: %lf ms\n", total_time / 100); ff;
-
+    execute(port, ip_address, measurements, 5, THROUGHTPUT);
     exit(0);
 }

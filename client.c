a4a501b2a2b88a0c24075d3bd4d197d1898df4ca
Giovanni Gobbi <ggobbi@Giovannis-MBP.homenet.telecomitalia.it>
2019-09-04 12:20:31 +0200

    Finish to implement client behaviour

--- client.c
+++ client.c
@@ -15,23 +15,42 @@
 #define RTT 1
 #define THROUGHPUT 2
 
+/**
+ * Defines an operation (RTT or THROUGHPUT) and some utils.
+ */
 typedef struct{
     int code;
     char * as_string;
+    char * measure_unit;
 } operation;
 
 /**
- * 
+ * Executes a measurement request.
+ * - operation: the operation (RTT or THROUGPUT)
+ * - sizes: the sizes of the various measurement probes
+ * - sizes_length: the number of sizes
+ * - n_measurements_for_size: the number of measurements for each size
  */
-int execute_request(operation operation, int sizes[], int sizes_length, int n_measurements_for_size, int server_delay);
+void execute_request(operation operation, int sizes[], int sizes_length, int n_measurements_for_size, int server_delay);
+
 /**
- * 
+ * Returns a string of length 'length' where all chars are set to 'a'.
+ * - length: the length of the string that will be returned
  */
 char * get_string_by_length(int length);
+
 /**
+ * Returns the average by operation. In particular:
+ * - if operation_code is RTT, it returns the average RTT from start to end
+ * - if operation_code is THROUGHPUT, it returns the througput computed as size / (avg_rtt from start to end)
  * 
+ * - operation_code: the operation code
+ * - rtts: the array that contains the rtts
+ * - start: the starting point of the avg computation
+ * - end: the ending point of the avg computation
+ * - size: the size of the messages 
  */
-double get_average_by_operation(int operation, long rtts[], int start, int end, int sizes[], int n_measurements_for_size);
+double get_average_by_operation(int operation_code, long rtts[], int start, int end, int size);
 
 /**
  * Returns the current time in milliseconds.
@@ -72,22 +91,25 @@ int main(int argc, char *argv[]) {
         operation op;
         op.code = RTT;
         op.as_string = "RTT";
+        op.measure_unit = "ms";
         execute_request(op, sizes, sizeof(sizes)/sizeof(int), n_measurements_for_size, server_delay);
     } else {
-        int sizes[] = {1000,2000,4000,16000,32000};
-        int n_measurements_for_size = 1;
+        int sizes[] = {1000, 2000, 4000, 16000, 32000};
+        int n_measurements_for_size = 20;
         operation op;
         op.code = THROUGHPUT;
         op.as_string = "Throughput";
+        op.measure_unit = "kbps";
         execute_request(op, sizes, sizeof(sizes)/sizeof(int), n_measurements_for_size, server_delay);
     }
 
     return 0;
 }
 
-int execute_request(operation operation, int sizes[], int sizes_length, int n_measurements_for_size, int server_delay) {
+void execute_request(operation operation, int sizes[], int sizes_length, int n_measurements_for_size, int server_delay) {
     int number_of_measurements = n_measurements_for_size * sizes_length;
     long rtts[number_of_measurements];
+    double total_average = 0;
 
     char *hello_operation = NULL;
     if(operation.code == RTT) {
@@ -95,21 +117,23 @@ int execute_request(operation operation, int sizes[], int sizes_length, int n_me
     } else {
         hello_operation = "thput";
     }
-    int max_size = max_array(sizes, sizes_length);
+    int max_size = max_array(sizes, sizes_length) + 50;
     // + 50 since the messages are longer than the max_size since they contain also the message fields
-    char *rcv_buffer = (char *) malloc(max_size + 50); 
-    check_allocation(rcv_buffer);
-    char *send_buffer = (char *) malloc(max_size + 50);
+    char *recv_buffer = (char *) malloc(max_size); 
+    check_allocation(recv_buffer);
+    char *send_buffer = (char *) malloc(max_size);
+    check_allocation(send_buffer);
     long long start, end;
+    ssize_t send_return_value;
     ssize_t recv_message_size;
     
     char *string = get_string_by_length(max_size);
 
     for(int i = 0; i < sizes_length; i++) {
-        int sfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-        if (sfd == -1) {
-            fprintf(stderr, "Socket initialization failed");
-            return 1;
+        int socket_file_descriptor = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (socket_file_descriptor == -1) {
+            perror("Socket initialization failed");
+            exit(EXIT_FAILURE);
         }
         
         struct sockaddr_in server_addr;
@@ -117,7 +141,7 @@ int execute_request(operation operation, int sizes[], int sizes_length, int n_me
         server_addr.sin_addr.s_addr = inet_addr(ip_address);
         server_addr.sin_port = htons(atoi(port));
         
-        int connect_return_value = connect(sfd, (struct sockaddr *) &server_addr, sizeof(server_addr));
+        int connect_return_value = connect(socket_file_descriptor, (struct sockaddr *) &server_addr, sizeof(server_addr));
         if (connect_return_value == -1) {
             perror("connect operation failed");
             exit(EXIT_FAILURE);
@@ -125,11 +149,15 @@ int execute_request(operation operation, int sizes[], int sizes_length, int n_me
         
         //Hello phase
         sprintf(send_buffer, "h %s %d %d %d", hello_operation, n_measurements_for_size, sizes[i], server_delay);
-        send(sfd, send_buffer, strlen(send_buffer), 0);
-        recv_message_size = recv(sfd, rcv_buffer, max_size, 0);
-        if(recv_message_size == -1 || strcmp(rcv_buffer, "200 OK - Ready") != 0) {
+        send_return_value = send(socket_file_descriptor, send_buffer, strlen(send_buffer), 0);
+        check_send(socket_file_descriptor, send_buffer, send_return_value);
+        send_buffer = memset(send_buffer, '\0', max_size);
+        recv_buffer = memset(recv_buffer, '\0', max_size);
+        recv_message_size = recv(socket_file_descriptor, recv_buffer, max_size, 0);
+        check_recv(socket_file_descriptor, recv_buffer, recv_message_size);
+        if(strcmp(recv_buffer, "200 OK - Ready") != 0) {
             fprintf(stderr, "Error: recv returned wrong string: "); ff;
-            print_string(stderr, rcv_buffer);
+            print_string(stderr, recv_buffer);
         }
 
         int size = sizes[i];
@@ -137,59 +165,64 @@ int execute_request(operation operation, int sizes[], int sizes_length, int n_me
         for(int j = 1; j <= n_measurements_for_size; j++) {
             sprintf(send_buffer, "m %d %.*s", j, size, string);
             start = get_time_in_milliseconds();
-            send(sfd, send_buffer, strlen(send_buffer), 0);
+            send_return_value = send(socket_file_descriptor, send_buffer, strlen(send_buffer), 0);
+            check_send(socket_file_descriptor, send_buffer, send_return_value);
+            send_buffer = memset(send_buffer, '\0', max_size);
             int tmp_recv_message_size = 0;
             recv_message_size = 0;
             while(recv_message_size != size && tmp_recv_message_size != -1) {
-                tmp_recv_message_size = recv(sfd, rcv_buffer, max_size, 0);
+                recv_buffer = memset(recv_buffer, '\0', max_size);
+                tmp_recv_message_size = recv(socket_file_descriptor, recv_buffer, max_size, 0);
+                check_recv(socket_file_descriptor, recv_buffer, tmp_recv_message_size);
                 recv_message_size += tmp_recv_message_size;
             }
             end = get_time_in_milliseconds();
             
             if(recv_message_size != size) {
                 fprintf(stderr, "Error: recv returned wrong string: "); ff;
-                print_string(stderr, rcv_buffer);
-                exit(1);
+                print_string(stderr, recv_buffer);
+                exit(EXIT_FAILURE);
             }
             rtts[i * n_measurements_for_size + j - 1] = end - start;
-            printf("Measured RTT for message of size %d: %ld ms.\n", size, rtts[i * n_measurements_for_size + j - 1]);
+            printf("Probe sequence numer: %d. Measured RTT: %ld ms.\n", (i * n_measurements_for_size + j), rtts[i * n_measurements_for_size + j - 1]);
         }
-        printf("Average %s for message of size %d: %lf\n", operation.as_string, size, get_average_by_operation(RTT, rtts, i * n_measurements_for_size, (i + 1) * n_measurements_for_size, sizes, n_measurements_for_size));
+        double average =  get_average_by_operation(operation.code, rtts, i * n_measurements_for_size, (i + 1) * n_measurements_for_size, size);
+        total_average += average;
+        printf("Average %s for message of size %d: %lf\n", operation.as_string, size, average);
         //Bye phase
         sprintf(send_buffer, "b");
-        send(sfd, send_buffer, max_size, 0);
-        recv_message_size = recv(sfd, rcv_buffer, max_size, 0);
-        printf("%zd ", recv_message_size);
-        print_string(stdout, rcv_buffer);
-        if(recv_message_size == -1 || strcmp(rcv_buffer, "200 OK - Closing") != 0) {
+        send_return_value = send(socket_file_descriptor, send_buffer, max_size, 0);
+        check_send(socket_file_descriptor, send_buffer, send_return_value);
+        send_buffer = memset(send_buffer, '\0', max_size);
+        
+        recv_buffer = memset(recv_buffer, '\0', max_size);
+        recv_message_size = recv(socket_file_descriptor, recv_buffer, max_size, 0);
+        check_recv(socket_file_descriptor, recv_buffer, recv_message_size);
+        if(strcmp(recv_buffer, "200 OK - Closing") != 0) {
             fprintf(stderr, "Error: recv returned wrong string: "); ff;
-            print_string(stderr, rcv_buffer);
+            print_string(stderr, recv_buffer);
+            exit(EXIT_FAILURE);
         }
-        close(sfd);
+        close(socket_file_descriptor);
     }
-
-    printf("Total Average %s: %lf", operation.as_string, get_average_by_operation(operation.code, rtts, 0, number_of_measurements, sizes, n_measurements_for_size));
     
-    return 0;
+    printf("Total Average %s: %lf %s.\n", operation.as_string, total_average / sizes_length, operation.measure_unit);
+    exit(EXIT_SUCCESS);
 }
 
-double get_average_by_operation(int operation_code, long rtts[], int start, int end, int sizes[], int n_measurements_for_size) {
+double get_average_by_operation(int operation_code, long rtts[], int start, int end, int size) {
+    long total_rtts = 0;
+    for(int i = start; i < end; i++) {
+        total_rtts += rtts[i];
+    }
+    double avg_rtt = (double) (total_rtts / (end - start));
     if(operation_code == RTT) {
-        long total_rtts = 0;
-        for(int i = start; i < end; i++) {
-            total_rtts += rtts[i];
-        }
-        return (double) (total_rtts / (end - start));
+        return avg_rtt;
     } else if(operation_code == THROUGHPUT) {
-        double total_throughputs = 0;
-        for(int i = start; i < end; i++) {
-            int size = sizes[i / n_measurements_for_size];
-            total_throughputs += (double) ((size * 1E6) / (rtts[i] / 1E3));
-        }
-        return total_throughputs / (end - start);
+        return (size / avg_rtt) / 1E3;
     } else {
         fprintf(stderr, "Unsupported operation.");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 }
 
@@ -198,7 +231,7 @@ char * get_string_by_length(int length) {
     string = (char *) malloc(length);
     if(string == NULL) {
         fprintf(stderr, "Malloc returned NULL");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     for(int i = 0; i < length; i++) {
